ff458ee429826d2102546bea8d86d3859cb77ecb
Anthony <ajtalvarez@gmail.com>
2018-01-16 13:48:27 -0500

    Literally a comma.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://entertheconnexus.herokuapp.com', 'https://connexustest.herokuapp.com' 'http://localhost:3001'
+    origins 'https://entertheconnexus.herokuapp.com', 'https://connexustest.herokuapp.com', 'http://localhost:3001'
 
     resource '*',
       headers: :any,

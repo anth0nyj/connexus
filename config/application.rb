e35fd69bf20ebcc869a6e6c9926708fcf1bcbaed
Anthony <ajtalvarez@gmail.com>
2018-01-16 11:14:57 -0500

    Commented out dotenv require.

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module CommunityAppApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    require "dotenv-rails"
+    # require "dotenv-rails"
   end
 end
